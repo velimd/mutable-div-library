4f9f781fa076bf1ff4f9c952b1df8a2a457bacd7
Ivan Toulou <touloumbadjiani@gmail.com>
2019-11-14 14:11:20 +0000

    removed commented logs

--- projects/mutable-div/src/lib/draggable-element.directive.ts
+++ projects/mutable-div/src/lib/draggable-element.directive.ts
@@ -20,20 +20,16 @@ export class DraggableElementDirective {
     this.mouseDown = true;
     console.log(this.mouseDown)
     this.setOriginalPosition(event);
-    // document.onmouseup = this.onMouseUp()
   }
 
   @HostListener('mousemove', ['$event']) onMouseMove(event: MouseEvent) {
     event.preventDefault();
     if (this.mouseDown) {
-      // console.log(this.mouseDown)
       const element = this.el.nativeElement;
       const newPosition = this.setNewPosition(event);
       this.setOriginalPosition(event);
       element.style.left = (element.offsetLeft + newPosition.x) + 'px';
       element.style.top = (element.offsetTop + newPosition.y) + 'px';
-      // console.log("element.style.left = " + element.style.left)
-      // console.log("element.style.top = " + element.style.top)
     }
   }
 
@@ -67,8 +63,6 @@ export class DraggableElementDirective {
           y: event.clientY
         };
     }
-    // console.log("Original Position: {x: " + this.originalPosition.x + ", y: " + this.originalPosition.y + "}")
-    // console.log("")curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}' https://hooks.slack.com/services/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
   }
 
   private setNewPosition(event: MouseEvent): Position {
@@ -101,7 +95,6 @@ export class DraggableElementDirective {
           y: event.clientY - this.originalPosition.y
         };
     }
-    // console.log("New Position: {x: " + newPosition.x + ", y: " + newPosition.y + "}")
     return newPosition;
   }
 }
