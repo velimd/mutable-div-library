38b1d9406f48814511f0c17ba0945231039bb15c
Ivan Toulou <touloumbadjiani@gmail.com>
2019-11-18 09:59:14 +0000

    clockwise and anticlockwise buttons rotate div 90 degrees

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mutable-div-library';
+
+  rotate = 0;
+
+  rotateClockwise() {
+    this.rotate = this.rotate + 90;
+    if (this.rotate === 360) {
+      this.rotate = 0
+    }
+  }
+
+  rotateAntiClockwise() {
+    if (this.rotate <= 0) {
+      this.rotate = 360
+    }
+    this.rotate = this.rotate - 90;
+  }
 }
