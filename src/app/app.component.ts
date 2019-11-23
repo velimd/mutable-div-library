18d1fc34825d6f486197c77dbef1e4150e254e52
velimd <duzgunce.vd@gmail.com>
2019-11-23 00:21:02 +0000

    adding zoom to test environment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+
+  @ViewChild('block', {static: false}) block: ElementRef;
+
+  rectangle = {
+    x: '20',
+    y: '180',
+    height: '150',
+    width: '100'
+  };
   rotation = 0;
+  zoom = 1;
+
+  constructor() {
+  }
+
+  ngAfterViewInit(): void {
+    this.rectangle.x = this.block.nativeElement.style.left;
+    this.rectangle.y = this.block.nativeElement.style.top;
+  }
 
   rotate90() {
     this.rotation = (this.rotation + 90) %360;
@@ -14,5 +32,9 @@ export class AppComponent {
 
   rotateC90() {
     this.rotation = (this.rotation + 270) %360;
+  }
+
+  stepZoom(value: number) {
+    this.zoom += value;
   }
 }
