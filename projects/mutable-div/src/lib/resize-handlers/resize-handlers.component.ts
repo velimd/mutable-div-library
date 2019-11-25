f0be8d9c7b30ef7941bb5ebf9d5fb7a91833d474
velimd <duzgunce.vd@gmail.com>
2019-11-25 23:16:40 +0000

    calculation for resizing for handles

--- projects/mutable-div/src/lib/resize-handlers/resize-handlers.component.ts
+++ projects/mutable-div/src/lib/resize-handlers/resize-handlers.component.ts
@@ -13,7 +13,6 @@ export class ResizeHandlersComponent implements AfterViewInit {
 
   @ViewChildren('handle') handles: QueryList<ElementRef>;
 
-  elementSize: Size;
   originalPosition: Position;
 
   constructor() { }
@@ -26,43 +25,64 @@ export class ResizeHandlersComponent implements AfterViewInit {
     this.handles.forEach((element, index) => {
       switch (index) {
         case 0:
-          element.nativeElement.style.top = '-5px';
           element.nativeElement.style.left = '-5px';
+          element.nativeElement.style.top = '-5px';
+          element.nativeElement.style.right = '';
+          element.nativeElement.style.bottom = '';
           break;
         case 1:
-          element.nativeElement.style.top = '-5px';
           element.nativeElement.style.right = '-5px';
+          element.nativeElement.style.top = '-5px';
+          element.nativeElement.style.left = '';
+          element.nativeElement.style.bottom = '';
           break;
         case 2:
-          element.nativeElement.style.bottom = '-5px';
           element.nativeElement.style.left = '-5px';
+          element.nativeElement.style.bottom = '-5px';
+          element.nativeElement.style.right = '';
+          element.nativeElement.style.top = '';
           break;
         case 3:
-          element.nativeElement.style.bottom = '-5px';
           element.nativeElement.style.right = '-5px';
+          element.nativeElement.style.bottom = '-5px';
+          element.nativeElement.style.left = '';
+          element.nativeElement.style.top = '';
           break;
       }
     });
   }
 
-  private setOriginalPosition(event: MouseEvent): void {
-    this.originalPosition = {
-      x: event.clientX,
-      y: event.clientY
-    };
-  }
-
   onMouseDown(event: MouseEvent) {
     event.stopPropagation();
-    this.setOriginalPosition(event);
   }
 
-  resize(className: string, event: MouseEvent) {
-    if (className.includes('BOTTOM-RIGHT')) {
-      this.parentElement.nativeElement.style.width =
-        this.parentElement.nativeElement.style.width + (event.clientX - this.originalPosition.x) + 'px';
-      this.parentElement.nativeElement.style.height =
-        this.parentElement.nativeElement.style.height + (event.clientY - this.originalPosition.y) + 'px';
+  resize(className: string, coordinates: Position) {
+    const parentElement = this.parentElement.nativeElement;
+    if (className.includes('TOP-LEFT')) {
+      parentElement.style.left = parentElement.offsetLeft + coordinates.x + 'px';
+      parentElement.style.top = parentElement.offsetTop + coordinates.y + 'px';
+      parentElement.style.width =
+        Math.max(parentElement.offsetWidth - coordinates.x, 20) + 'px';
+      parentElement.style.height =
+        Math.max(parentElement.offsetHeight - coordinates.y, 20) + 'px';
+    } else if (className.includes('TOP-RIGHT')) {
+      parentElement.style.width =
+        Math.max(parentElement.offsetWidth + coordinates.x, 20) + 'px';
+      parentElement.style.top = parentElement.offsetTop + coordinates.y + 'px';
+      parentElement.style.height =
+        Math.max(parentElement.offsetHeight - coordinates.y, 20) + 'px';
+    } else if (className.includes('BOTTOM-LEFT')) {
+      parentElement.style.left = parentElement.offsetLeft + coordinates.x + 'px';
+      parentElement.style.width =
+        Math.max(parentElement.offsetWidth - coordinates.x, 20) + 'px';
+      parentElement.style.height =
+        Math.max(parentElement.offsetHeight + coordinates.y, 20) + 'px';
+    } else if (className.includes('BOTTOM-RIGHT')) {
+      parentElement.style.width =
+        Math.max(parentElement.offsetWidth + coordinates.x, 20) + 'px';
+      parentElement.style.height =
+        Math.max(parentElement.offsetHeight + coordinates.y, 20) + 'px';
     }
+    this.setHandlePositions();
   }
 }
