acf91ddff08197dc52a1d2cf8335a4818750bd7a
velimd <velican.duzgunce@solirius.com>
2019-11-26 11:01:41 +0000

    refactoring resize function (2)

--- projects/mutable-div/src/lib/resize-handlers/resize-handlers.component.ts
+++ projects/mutable-div/src/lib/resize-handlers/resize-handlers.component.ts
@@ -1,5 +1,6 @@
 import {AfterViewInit, Component, ElementRef, Input, OnInit, QueryList, ViewChildren} from '@angular/core';
 import {Position} from '../model/position.model';
+import {Size} from '../model/size.model';
 
 @Component({
   selector: 'lib-resize-handlers',
@@ -24,26 +25,26 @@ export class ResizeHandlersComponent implements AfterViewInit {
     this.handles.forEach((element, index) => {
       switch (index) {
         case 0:
-          element.nativeElement.style.left = '-5px';
-          element.nativeElement.style.top = '-5px';
+          element.nativeElement.style.left = '-7px';
+          element.nativeElement.style.top = '-7px';
           element.nativeElement.style.right = '';
           element.nativeElement.style.bottom = '';
           break;
         case 1:
-          element.nativeElement.style.right = '-5px';
-          element.nativeElement.style.top = '-5px';
+          element.nativeElement.style.right = '-7px';
+          element.nativeElement.style.top = '-7px';
           element.nativeElement.style.left = '';
           element.nativeElement.style.bottom = '';
           break;
         case 2:
-          element.nativeElement.style.left = '-5px';
-          element.nativeElement.style.bottom = '-5px';
+          element.nativeElement.style.left = '-7px';
+          element.nativeElement.style.bottom = '-7px';
           element.nativeElement.style.right = '';
           element.nativeElement.style.top = '';
           break;
         case 3:
-          element.nativeElement.style.right = '-5px';
-          element.nativeElement.style.bottom = '-5px';
+          element.nativeElement.style.right = '-7px';
+          element.nativeElement.style.bottom = '-7px';
           element.nativeElement.style.left = '';
           element.nativeElement.style.top = '';
           break;
@@ -57,46 +58,31 @@ export class ResizeHandlersComponent implements AfterViewInit {
 
   resize(className: string, coordinates: Position) {
     const parentElement = this.parentElement.nativeElement;
-    if (className.includes('TOP-LEFT')) {
-      const width = parentElement.offsetWidth - coordinates.x;
-      const height = parentElement.offsetHeight - coordinates.y;
-      if (width > this.minimumSize) {
-        parentElement.style.left = parentElement.offsetLeft + coordinates.x + 'px';
-        parentElement.style.width = width + 'px';
-
+    const size: Size = {
+      width: className.includes('LEFT') ? parentElement.offsetWidth - coordinates.x : parentElement.offsetWidth + coordinates.x,
+      height: className.includes('TOP') ?  parentElement.offsetHeight - coordinates.y :  parentElement.offsetHeight + coordinates.y,
+      position: {
+        x: parentElement.offsetLeft + coordinates.x,
+        y: parentElement.offsetTop + coordinates.y
       }
-      if (height > this.minimumSize) {
-        parentElement.style.top = parentElement.offsetTop + coordinates.y + 'px';
-        parentElement.style.height = height + 'px';
-      }
-    } else if (className.includes('TOP-RIGHT')) {
-      const width = parentElement.offsetWidth + coordinates.x;
-      const height = parentElement.offsetHeight - coordinates.y;
-      if (width > this.minimumSize) {
-        parentElement.style.width = width + 'px';
-      }
-      if (height > this.minimumSize) {
-        parentElement.style.top = parentElement.offsetTop + coordinates.y + 'px';
-        parentElement.style.height = height + 'px';
-      }
-    } else if (className.includes('BOTTOM-LEFT')) {
-      const width = parentElement.offsetWidth - coordinates.x;
-      const height = parentElement.offsetHeight + coordinates.y;
-      if (width > this.minimumSize) {
-        parentElement.style.left = parentElement.offsetLeft + coordinates.x + 'px';
-        parentElement.style.width = width + 'px';
-      }
-      if (height > this.minimumSize) {
-        parentElement.style.height = height + 'px';
-      }
-    } else if (className.includes('BOTTOM-RIGHT')) {
-      const width = parentElement.offsetWidth + coordinates.x;
-      const height = parentElement.offsetHeight + coordinates.y;
-      if (width > this.minimumSize) {
-        parentElement.style.width = width + 'px';
-      }
-      if (height > this.minimumSize) {
-        parentElement.style.height = height + 'px';
+    };
+    if (size.width > this.minimumSize && size.height > this.minimumSize) {
+      if (className.includes('TOP-LEFT')) {
+        parentElement.style.left = size.position.x + 'px';
+        parentElement.style.width = size.width + 'px';
+        parentElement.style.top =  size.position.y + 'px';
+        parentElement.style.height = size.height + 'px';
+      } else if (className.includes('TOP-RIGHT')) {
+        parentElement.style.width = size.width + 'px';
+        parentElement.style.top = size.position.y + 'px';
+        parentElement.style.height = size.height + 'px';
+      } else if (className.includes('BOTTOM-LEFT')) {
+        parentElement.style.left = size.position.x + 'px';
+        parentElement.style.width = size.width + 'px';
+        parentElement.style.height = size.height + 'px';
+      } else {
+        parentElement.style.width = size.width + 'px';
+        parentElement.style.height = size.height + 'px';
       }
     }
     this.setHandlePositions();
